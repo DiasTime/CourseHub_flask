8446df01e5515d8f420c1a63bcfcfd3b22efd400
DiasTime <36094372+DiasTime@users.noreply.github.com>
2024-04-07 15:25:45 +0500

    ss

---
+++ app3.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, jsonify
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import random
+
+app = Flask(__name__)
+
+# Initialize Firestore with your credentials
+cred = credentials.Certificate("firestore.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+def get_course_documents():
+    courses_ref = db.collection('courses').get()
+    courses = [course_doc.to_dict() for course_doc in courses_ref]
+    return courses
+
+@app.route('/')
+def index():
+    return render_template('available_courses.html')
+
+@app.route('/courses')
+def courses():
+    courses = get_course_documents()
+    return jsonify(courses)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5002)
--- frontend/src/pages/NotFound.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function NotFound() {
-  return <div className="wrapper">NotFound</div>;
-}
-
-export default NotFound;
